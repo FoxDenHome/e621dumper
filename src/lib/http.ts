51c302b0f0d9b51c0301a62e62b75c000cf3d651
Doridian <git@doridian.net>
2022-09-14 20:18:07 -0700

    forgot add

---
+++ src/lib/http.ts
@@ -0,0 +1,25 @@
+
+import { request, Agent, RequestOptions } from 'https';
+import { IncomingMessage } from 'http';
+
+const agent = new Agent({ keepAlive: true });
+
+export function requestPromise(url: string, options: RequestOptions): Promise<IncomingMessage> {
+    return new Promise((resolve, reject) => {
+    	request(url, { agent, ...options }, resolve).on('error', reject).end();
+    });
+}
+
+export async function requestPromiseReadBody(url: string, options: RequestOptions): Promise<string> {
+    const res = await requestPromise(url, options);
+
+    return new Promise((resolve) => {
+        const data: string[] = [];
+        res.on('data', (d) => {
+            data.push(d);
+        });
+        res.on('end', () => {
+            resolve(data.join());
+        });
+    });
+}
