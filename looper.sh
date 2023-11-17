ccf45e8fad75d35839d8e41f84b5c992d81e668d
Doridian <git@doridian.net>
2023-11-17 09:11:13 -0800

    change sleeps

--- looper.sh
+++ looper.sh
@@ -4,26 +4,29 @@ echo 'Fetch'
 until node dist/bin/fetchnew.js
 do
 	echo 'Retrying fetch'
-	sleep 5
+	sleep 10
 done
+sleep 10
 
 echo 'DL file'
 until node dist/bin/downloadfiles.js --type=file --looper --pauser=/config/pauser
 do
 	echo 'Retrying DL file'
-	sleep 5
+	sleep 10
 done
+sleep 10
 
 echo 'DL sample'
 until node dist/bin/downloadfiles.js --type=sample --looper --pauser=/config/pauser
 do
 	echo 'Retrying DL sample'
-	sleep 5
+	sleep 10
 done
+sleep 10
 
 echo 'DL preview'
 until node dist/bin/downloadfiles.js --type=preview --looper --pauser=/config/pauser
 do
 	echo 'Retrying DL preview'
-	sleep 5
+	sleep 10
 done
