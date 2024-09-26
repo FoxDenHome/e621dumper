4399cb98efb9fff0504fde1e647a8e0b645df670
Doridian <git@doridian.net>
2024-09-26 09:11:31 -0700

    Stop downloading sample and preview, we got the actual data

--- looper.sh
+++ looper.sh
@@ -6,27 +6,27 @@ do
 	echo 'Retrying fetch'
 	sleep 10
 done
-sleep 10
 
+sleep 10
 echo 'DL file'
 until node dist/bin/downloadfiles.js --type=file --looper
 do
 	echo 'Retrying DL file'
 	sleep 10
 done
-sleep 10
 
-echo 'DL sample'
-until node dist/bin/downloadfiles.js --type=sample --looper
-do
-	echo 'Retrying DL sample'
-	sleep 10
-done
-sleep 10
+#sleep 10
+# echo 'DL sample'
+# until node dist/bin/downloadfiles.js --type=sample --looper
+# do
+# 	echo 'Retrying DL sample'
+# 	sleep 10
+# done
 
-echo 'DL preview'
-until node dist/bin/downloadfiles.js --type=preview --looper
-do
-	echo 'Retrying DL preview'
-	sleep 10
-done
+# sleep 10
+# echo 'DL preview'
+# until node dist/bin/downloadfiles.js --type=preview --looper
+# do
+# 	echo 'Retrying DL preview'
+# 	sleep 10
+# done
