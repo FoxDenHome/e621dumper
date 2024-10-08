ed6e8bc7440e2fd9ba94128ae28d2c45328f2334
Doridian <git@doridian.net>
2024-10-07 20:39:22 -0700

    Handle invalid JSON

--- src/api/index.ts
+++ src/api/index.ts
@@ -90,7 +90,14 @@ app.get('/api/v1/posts', async (req: express.Request, res: express.Response) => 
 });
 
 app.post('/api/v1/posts', async (req: express.Request, res: express.Response) => {
-    const query = JSON.parse(req.body);
+    let query: Record<string, unknown>;
+    try {
+        query = JSON.parse(req.body as string) as Record<string, unknown>;
+    } catch {
+        res.status(400).send({ error: 'Invalid JSON' });
+        return;
+    }
+
     res.send(await processSearch(query, req));
 });
 
