c4d4af17baaaf928bd4867b84beed47d6f802a69
Mark Dietzer <git@doridian.net>
2025-05-02 12:55:11 -0700

    reindex.sh (#319)

---
+++ esjson/reindex.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Make sure to change the numbers below!
+
+curl -v -XPOST "$(hostname):9200/_reindex" -H 'Content-Type: application/json' --data-raw '{
+  "source": {
+    "index": "e621posts_1"
+  },
+  "dest": {
+    "index": "e621posts_2"
+  }
+}'
+
+curl -v -XPOST "$(hostname):9200/_aliases" -H 'Content-Type: application/json' --data-raw '{
+    "actions" : [
+        { "add" : { "index" : "e621posts_2", "alias" : "e621posts" } }
+    ]
+}'
