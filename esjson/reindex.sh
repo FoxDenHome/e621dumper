0488c573dd09bcf8483d70dd52df6ece7bee7265
Doridian <git@doridian.net>
2025-05-02 13:20:39 -0700

    dynamic

--- esjson/reindex.sh
+++ esjson/reindex.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 # Make sure to change the numbers below!
+OLDVER=1
+NEWVER=2
 
-curl -v -XPOST "$(hostname):9200/_reindex" -H 'Content-Type: application/json' --data-raw '{
-  "source": {
-    "index": "e621posts_1"
+curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
+  \"source\": {
+    \"index\": \"e621posts_${OLDVER}\"
   },
-  "dest": {
-    "index": "e621posts_2"
+  \"dest\": {
+    \"index\": \"e621posts_${NEWVER}\"
   }
-}'
+}"
 
-curl -v -XPOST "$(hostname):9200/_aliases" -H 'Content-Type: application/json' --data-raw '{
-    "actions" : [
-        { "add" : { "index" : "e621posts_2", "alias" : "e621posts" } }
+curl -v -XPOST 'http://elasticsearch:9200/_aliases' -H 'Content-Type: application/json' --data-raw "{
+    \"actions\" : [
+        { \"add\": { \"index\": \"e621posts_${NEWVER}\", \"alias\": \"e621posts\" } },
+        { \"remove\": { \"index\" : \"e621posts_${OLDVER}\", \"alias\": \"e621posts\" } }
     ]
-}'
+}"
+
+echo 'Run the following commands to delete the old indices:'
+echo "curl -XDELETE 'http://elasticsearch:9200/e621posts_${OLDVER}'"
