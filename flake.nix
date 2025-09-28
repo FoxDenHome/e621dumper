5be786a88684cedacf17d33395d87f2a2013db81
Doridian <git@doridian.net>
2025-09-27 19:50:37 -0700

    Make this a nix flake

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
             pname = "e621dumper";
             version = "1.0.0";
             src = ./.;
-            npmDepsHash = "sha256-6n0mrXEmURTfy6tRc9II75znxh7wWB0u4u1xHC3GnSU=";
+            npmDepsHash = "sha256-U5aLcy0HJgwJ9rZehSf/7UViXBD1nUN3Y3X+m6OrqJI=";
           };
         };
       });
