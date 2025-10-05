0d62de4f028c09ac7e3aca54ef5e5e494068bb4a
Doridian <git@doridian.net>
2025-10-04 23:17:46 -0700

    autofetch package info

--- flake.nix
+++ flake.nix
@@ -3,22 +3,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }: let
+    packageJson = nixpkgs.lib.trivial.importJSON ./package.json;
+  in
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         package = pkgs.buildNpmPackage {
-            pname = "e621dumper";
-            version = "1.0.0";
-            src = ./.;
-            npmDeps = pkgs.importNpmLock { npmRoot = ./.; };
-            npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-          };
+          pname = packageJson.name;
+          version = packageJson.version;
+          src = ./.;
+          npmDeps = pkgs.importNpmLock { npmRoot = ./.; };
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+        };
       in
       {
-        packages = {
-          default = package;
-          e621dumper = package;
-        };
+        packages.default = package;
+        packages.${packageJson.name} = package;
       });
 }
